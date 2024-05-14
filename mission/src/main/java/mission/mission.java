6b86cbf733550ec4b218f86962fb180ed7eaacb8
Lime <ruxkil238@gmail.com>
2024-05-14 11:58:40 +0900

    modify1

---
+++ mission/src/main/java/mission/mission.java
@@ -0,0 +1,18 @@
+package mission;
+
+public class mission {
+
+
+	public static void main (String[] args) {
+		
+		
+		
+		for(int i=1; i<11; i++) {
+			System.out.println(i);
+		}
+	}
+	
+	
+	
+	
+}
